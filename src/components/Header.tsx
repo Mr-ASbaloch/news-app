b7cdb467278fccc6b59f7957a1b1eb94b29550b8
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2024-01-19 11:37:20 +0500

    commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import vector from "../assets/Vector.png";
+import search from "../assets/search.png";
+import menu from "../assets/menu.png";
+const Header: React.FC = () => {
+  return (
+    <div className="main-div px-3 md:px-7 lg:px-[7%] ">
+      <div className="flex justify-between items-center mt-3">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className=" hidden md:flex items-center justify-between  gap-5 font-semibold capitalize ">
+          <a href="">Corona Updates</a>
+          <a href="">Politics</a>
+          <a href="">Business</a>
+          <a href="">Travel</a>
+          <a href="">world</a>
+          <a href="">podcasts</a>
+        </div>
+
+        <div className="flex justify-between items-center gap-3">
+          <img src={vector} alt="logo" />
+          <img src={search} alt="logo" />
+          <img src={menu} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
